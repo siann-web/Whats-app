b60dc2e649e7e19f631f68f464cd4e8a6bf28677
370100961 <370100961@prof.unijuazeiro.edu.br>
2024-05-09 18:25:02 -0300

    Base

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import MyComponent from './components/ MyComponent';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <MyComponent />
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
